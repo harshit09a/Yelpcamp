be798315ffa3b6630992a32042196b4e20e514f9
HARSHIT GARG <63994091+harshit09a@users.noreply.github.com>
2020-12-30 01:13:08 +0530

    Update collection.js

--- DB/collection.js
+++ DB/collection.js
@@ -1,5 +1,5 @@
 var mongoose = require("mongoose");
-var URI = "mongodb+srv://harshit09a:97@Harshit@cluster0.ptuyu.mongodb.net/yelp_camp?retryWrites=true&w=majority"
+var URI = "mongodb+srv://harshit09a:<password>@cluster0.ptuyu.mongodb.net/yelp_camp?retryWrites=true&w=majority"
 var connectDB = async()=>{
     await mongoose.connect(URI,{
         useNewUrlParser:true,
