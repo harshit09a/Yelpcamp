29764caf500f3b3583f88c97ef0d5277e12ece40
HARSHIT GARG <63994091+harshit09a@users.noreply.github.com>
2020-12-30 01:05:47 +0530

    Update app.js
    port address

--- app.js
+++ app.js
@@ -398,6 +398,6 @@ function checkcomment(req,res,next)
 
 
 
-app.listen(3000, process.env.IP, function(){
+app.listen(process.env.PORT||3000, process.env.IP, function(){
    console.log("The YelpCamp Server Has Started!");
 });
